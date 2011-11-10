71f901b7d62b1d010a64a72b504b69e6ff0eed53
A. Murat Eren <a.murat.eren@gmail.com>
2011-11-09 19:51:48 -0500

    better visualization

--- entropy.py
+++ entropy.py
@@ -6,6 +6,38 @@ import operator
 from scipy import log2 as log
 import numpy as np
 
+import fastalib as u
+
+COLORS = {'A': 'red',
+          'T': 'blue', 
+          'C': 'green', 
+          'G': 'purple', 
+          'N': 'white', 
+          '-': '#CACACA'}
+
+def get_consensus_sequence(alignment_file):
+    consensus_sequence = ''
+    fasta = u.SequenceSource(alignment_file)
+    
+    fasta.next()
+    alignment_length = len(fasta.seq)
+    
+    consensus_dict = {}
+    
+    for i in range(0, alignment_length):
+        consensus_dict[i] = {'A': 0, 'T': 0, 'C': 0, 'G': 0, '-': 0}
+    
+    fasta.reset()
+    
+    while fasta.next():
+        for pos in range(0, alignment_length):
+            consensus_dict[pos][fasta.seq[pos]] += 1
+    
+    for pos in range(0, alignment_length):
+        consensus_sequence += sorted(consensus_dict[pos].iteritems(), key=operator.itemgetter(1), reverse=True)[0][0]
+    
+    return consensus_sequence
+
 def entropy(l):
     p_a = (len([x for x in l if x.upper() == 'A']) * 1.0 / len(l)) + 0.0000000000000000001
     p_t = (len([x for x in l if x.upper() == 'T']) * 1.0 / len(l)) + 0.0000000000000000001
@@ -18,6 +50,8 @@ def entropy(l):
 lines = [l for l in open(sys.argv[1]) if not l.startswith('>')]
 
 entropy_tpls = []
+
+consensus_sequence = get_consensus_sequence(sys.argv[1])
 
 start = 0
 end = len(lines[0])
@@ -35,7 +69,6 @@ for i in range(start, end):
             entropy_tpls.append((i, 0.0),)
         else:
             entropy_tpls.append((i, e),)
-            #print i, e
 print
 
 stuff = [x[1] for x in entropy_tpls]
@@ -44,7 +77,7 @@ def draw(stuff):
     import matplotlib.pyplot as plt
     import numpy as np
 
-    fig = plt.figure(figsize = (10, 2))
+    fig = plt.figure(figsize = (len(consensus_sequence) / 20, 10))
 
     plt.rcParams.update({'axes.linewidth' : 0.1})
     plt.rc('grid', color='0.70', linestyle='-', linewidth=0.1)
@@ -53,9 +86,16 @@ def draw(stuff):
     plt.subplots_adjust(hspace = 0, wspace = 0, right = 0.995, left = 0.050, top = 0.92, bottom = 0.10)
 
     ax = fig.add_subplot(111)
+
+    y_maximum = max(stuff) + (max(stuff) / 10.0)
+    for i in range(0, len(consensus_sequence)):
+        for y in range(int(y_maximum * 100), 0, -3):
+            plt.text(i, y / 100.0, consensus_sequence[i],  alpha = y / (y_maximum * 100.0), fontsize = 5, color = COLORS[consensus_sequence[i]])
+
     ind = np.arange(len(stuff))
     ax.bar(ind, stuff, color = 'black', lw = 0.5)
     ax.set_xlim([0, end])
+    ax.set_ylim([0, y_maximum])
     plt.savefig(sys.argv[1] + '-ENTROPY.png')
     plt.show()
 
