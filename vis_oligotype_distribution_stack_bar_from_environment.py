7fd63a25220cdfb635f7e58fc4e3a5cb05a20426
A. Murat Eren <a.murat.eren@gmail.com>
2011-11-17 15:54:12 -0500

    add a fixme for viamics dependency, remove unnecessary comment lines..

--- vis_oligotype_distribution_stack_bar_from_environment.py
+++ vis_oligotype_distribution_stack_bar_from_environment.py
@@ -18,8 +18,12 @@ import cPickle
 import sys
 from scipy import stats
 
-samples_dict = cPickle.load(open(sys.argv[1])) #samples_dict_serialized as a parameter..
-oligos_color_dict = cPickle.load(open(sys.argv[2])) # taxon_color_dict from viamics
+# samples_dict_serialized as a parameter..
+samples_dict = cPickle.load(open(sys.argv[1])) 
+
+# taxon_color_dict from viamics
+oligos_color_dict = cPickle.load(open(sys.argv[2]))
+# FIXME: This viamics dependency should not be here.
 
 samples = [s for s in samples_dict.keys()]
 samples.sort()
@@ -50,8 +54,6 @@ for sample in samples:
         o.append(oligo_abundance * 100.0 / total_oligos_in_sample)
     samples_oligo_vectors_percent_normalized[sample] = o
 
-
-# lets seee..
 fig = plt.figure(figsize=(20, 10))
 
 plt.subplots_adjust(left=0.03, bottom = 0.15, top = 0.97, right = 0.90)
@@ -59,8 +61,8 @@ plt.subplots_adjust(left=0.03, bottom = 0.15, top = 0.97, right = 0.90)
 fig.canvas.set_window_title('Sjiz')
 
 N = len(samples)
-ind = np.arange(N)    # the x locations for the groups
-width = 0.75  # the width of the bars: can also be len(x) sequence
+ind = np.arange(N)
+width = 0.75
 
 bars = []
 
@@ -85,16 +87,13 @@ plt.xlim(xmin = -(width) / 2, xmax = len(samples))
 plt.legend([b[0] for b in bars][::-1], oligos[::-1], bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.0, shadow=True, fancybox=True)
 
 leg = plt.gca().get_legend()
-ltext  = leg.get_texts()  # all the text.Text instance in the legend
-llines = leg.get_lines()  # all the lines.Line2D instance in the legend
-frame  = leg.get_frame()  # the patch.Rectangle instance surrounding the legend
+ltext  = leg.get_texts()
+llines = leg.get_lines()
+frame  = leg.get_frame()
 
-# see text.Text, lines.Line2D, and patches.Rectangle for more info on
-# the settable properties of lines, text, and rectangles
-frame.set_facecolor('0.80')      # set the frame face color to light gray
-plt.setp(ltext, fontsize='small', fontname='arial', family='monospace')    # the legend text fontsize
-plt.setp(llines, linewidth=1.5)      # the legend linewidth
-
+frame.set_facecolor('0.80')
+plt.setp(ltext, fontsize='small', fontname='arial', family='monospace')
+plt.setp(llines, linewidth=1.5)
 
 plt.savefig(sys.argv[1] + '-SBC.png')
 plt.show()
