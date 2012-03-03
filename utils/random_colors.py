c4536adfeef89d622ebc99a52c1792b0aaf199e8
A. Murat Eren <a.murat.eren@gmail.com>
2012-03-03 14:28:41 -0500

    utility for random color generation..

---
+++ utils/random_colors.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2010 - 2012, A. Murat Eren
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+
+import os
+import sys
+import random
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+#
+# all available colormaps in matplotlib can be seen via
+# http://www.scipy.org/Cookbook/Matplotlib/Show_colormaps
+#
+
+def random_colors(oligotypes, output_file_path = None, colormap = 'Paired'):
+    def getColor(name, n):
+        return cm.get_cmap(name, lut=n+2)
+
+    color_dict = {}
+
+    random.shuffle(oligotypes)
+    colors = getColor(colormap, len(oligotypes))
+    for i in range(0, len(oligotypes)):
+        rgba_color = colors(i)
+        hex_color = '#'
+        for t in rgba_color[0:3]:
+            h = str(hex(int(t * 255)))[2:]
+            hex_color += '00' if h == '0' else h
+        color_dict[oligotypes[i]] = hex_color
+
+    if output_file_path:
+        open(output_file_path, 'w').write('\n'.join(['%s\t%s' % (c, color_dict[c]) for c in color_dict]) + '\n')
+    
+    return color_dict
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Generate Random Colors')
+    parser.add_argument('oligotypes', metavar = 'FASTA_FILE', help = 'Oligotype sequences in FASTA format')
+    parser.add_argument('--output-file', default = None, metavar = 'OUTPUT_FILE',\
+                        help = 'File name to store random colors')
+    parser.add_argument('--colormap', default = 'Accent', metavar = 'MATPLOTLOB_COLORMAP',\
+                        help = 'see http://www.scipy.org/Cookbook/Matplotlib/Show_colormaps')
+
+
+    args = parser.parse_args()
+
+    oligotypes = [line.strip() for line in open(parser.parse_args().oligotypes) if not line.startswith('>')]
+    
+    colors_dict = random_colors(oligotypes, output_file_path = args.output_file, colormap = args.colormap) 
+
+    if not args.output_file:
+        for oligo in colors_dict:
+            print '%s: %s' % (oligo, colors_dict[oligo])
+
