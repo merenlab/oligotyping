639b52d42c701fa2f3c41e0147e3a9c1c1c2561c
A. Murat Eren <a.murat.eren@gmail.com>
2012-03-17 12:03:04 -0400

    fixy

--- utils/trim_uninformative_columns_from_alignment.py
+++ utils/trim_uninformative_columns_from_alignment.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 
-sys.path.append('../lib')
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../lib'))
 import fastalib as u
 
 # eh..
