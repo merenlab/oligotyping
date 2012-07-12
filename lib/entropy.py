5349bfea83726ad96f5e22896a285b6eb9cb06eb
A. Murat Eren <a.murat.eren@gmail.com>
2012-07-11 23:27:51 -0400

    better structure

---
+++ lib/entropy.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2010 - 2012, A. Murat Eren
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+
+import os
+import sys
+import numpy
+import operator
+import cPickle
+from scipy import log2 as log
+
+import lib.fastalib as u
+from utils import parsers
+from utils.utils import process_command_line_args_for_quality_files
+from utils.random_colors import get_list_of_colors
+from utils.utils import pretty_print
+from utils.utils import Progress
+from utils.utils import Run
+from utils.utils import P
+
+
+class EntropyError(Exception):
+    def __init__(self, e = None):
+        Exception.__init__(self)
+        self.e = e
+        return
+    def __str__(self):
+        return 'Error: %s' % self.e
+
+
+VALID_CHARS = set(['A', 'T', 'C', 'G', '-'])
+
+run = Run()
+progress = Progress()
+
+
+def entropy(l, l_qual = None, expected_qual_score = 40):
+    l = l.upper()
+    
+    E_Cs = []
+    for char in VALID_CHARS:
+        P_C = (l.count(char) * 1.0 / len(l)) + 0.0000000000000000001
+        E_Cs.append(P_C * log(P_C))
+   
+    if l_qual:
+        # return weighted entropy
+        return -(sum(E_Cs) * (l_qual['mean'] / expected_qual_score))
+    else:
+        # return un-weighted entropy
+        return -(sum(E_Cs))
+
+
+def entropy_analysis(alignment_path, output_file = None, verbose = True, uniqued = False, freq_from_defline = None, weighted = False, qual_stats_dict = None):
+    if freq_from_defline == None:
+        freq_from_defline = lambda x: int([t.split(':')[1] for t in x.split('|') if t.startswith('freq')][0])
+
+    lines = []
+    previous_alignment_length = None
+   
+    alignment = u.SequenceSource(alignment_path)
+
+    progress.new('Processing the Alignment')
+
+    # processing the alignment file..
+    while alignment.next():
+        # check the alignment lengths along the way:
+        if previous_alignment_length:
+            if previous_alignment_length != len(alignment.seq):
+                raise EntropyError, "Not all reads have the same length."
+
+        # print out process info
+        if verbose:
+            if alignment.pos % 10000 == 0:
+                progress.update('Reads processed: %s' % (pretty_print(alignment.pos)))
+        
+        # fill 'lines' variable
+        if not uniqued:
+            lines.append(alignment.seq)
+        else:
+            frequency = freq_from_defline(alignment.id)
+            for i in range(0, frequency):
+                lines.append(alignment.seq)
+
+        previous_alignment_length = len(alignment.seq)
+
+    if verbose:
+        progress.end()
+        run.info('Number of reads', pretty_print(alignment.pos))
+
+    alignment.close()
+
+
+    # entropy analysis
+    entropy_tpls = []
+    progress.new('Entropy Analysis')
+    for position in range(0, len(lines[0])):
+        if verbose:
+            progress.update(P(int(position + 1), len(lines[0])))
+   
+        if len(set([x[position] for x in lines])) == 1:
+            entropy_tpls.append((position, 0.0),)
+        else:
+            column = "".join([x[position] for x in lines])
+
+            if weighted:
+                if not qual_stats_dict: 
+                    raise EntropyError, "Weighted entropy is selected, but no qual stats are provided"
+                e = entropy(column, l_qual = qual_stats_dict[position])
+            else:
+                e = entropy(column)
+
+            if e < 0.00001:
+                entropy_tpls.append((position, 0.0),)
+            else:
+                entropy_tpls.append((position, e),)
+
+    sorted_entropy_tpls = sorted(entropy_tpls, key=operator.itemgetter(1), reverse=True)
+    entropy_components_larger_than_0 = [e[1] for e in entropy_tpls if e[1] > 0]
+
+    if verbose:
+        progress.end()
+        run.info('Entropy analysis', 'Done (total of %d components greater than 0, mean: %.2f, max: %.2f, min: %.2f).' \
+                                                        % (len(entropy_components_larger_than_0),
+                                                           numpy.mean(entropy_components_larger_than_0),
+                                                           numpy.max(entropy_components_larger_than_0),
+                                                           numpy.min(entropy_components_larger_than_0)))
+   
+    if output_file:
+        entropy_output = open(output_file, 'w')
+        for _component, _entropy in sorted_entropy_tpls:
+            entropy_output.write('%d\t%.4f\n' % (_component, _entropy))
+        if verbose:
+            run.info('Entropy analysis output file path', output_file)
+        entropy_output.close()
+    
+    return [x[1] for x in entropy_tpls]
+
+
---
+++ utils/parsers.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+# -*- coding: utf-8
+
+# Copyright (C) 2010 - 2012, A. Murat Eren
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+
+import argparse
+
+
+def oligotyping():
+    parser = argparse.ArgumentParser(description='Start an Oligotyping Process')
+    parser.add_argument('alignment', metavar = 'INPUT ALIGNMENT',
+                        help = 'Alignment file that contains all datasets and sequences in FASTA format')
+    parser.add_argument('entropy', metavar = 'ENTROPY',
+                        help = 'File that contains the columns and the entropy values computer previously')
+    parser.add_argument('-o', '--output-directory', help = 'Output directory', default = None)
+    parser.add_argument('-c', '--number-of-auto-components', type=int, default=None,
+                        help = 'Number of components to use from alignment to generate oligotypes. Default\
+                                is "5", which is a completely arbitrary value. Number of components should\
+                                be determined after a careful examination of entropy figure.')
+    parser.add_argument('--qual-scores-file', metavar = 'QUAL SCORES FILE',
+                        help = 'FASTA formatted file that contains PHRED base call values\
+                                for each read in the alignment file')
+    parser.add_argument('--qual-scores-dict', metavar = 'QUAL SCORES DICT',
+                        help = 'Previously computed and serialized dictionary that contains\
+                                PHRED base call values for each read in the alignment file. If you\
+                                provide --qual-scores-file, that file will be used to recompute this\
+                                dictionary and the file you refer with this parameter will\
+                                not be ignored')
+    parser.add_argument('--qual-stats-dict', metavar = 'QUAL STATS DICT',
+                        help = 'Previously computed and serialized dictionary that contains\
+                                PHRED base call quality score statistics for the alignment file. If\
+                                you provide --qual-scores-dict, it will be used to recompute this\
+                                dictionary and the file you refer to with this parameter will\
+                                actually not be used')
+    parser.add_argument('-q', '--min-base-quality', type=int, default=15,
+                        help = 'Minimum quality score for each base in locations of interest of a read to be\
+                                considered in an oligotype. When base quality score files are provided, this\
+                                value makes sure that low quality bases that are more likely to be the result\
+                                of random sequencing errors do not create artificial oligotypes. Any read that has\
+                                less quality score than the given value, will simply be discarded. This parameter\
+                                only in effect when --qual-scores-file or --qual-scores-dict parameters are used. \
+                                Defeault --min-base-quality is 15.')
+    parser.add_argument('-C', '--selected-components', type=str, default=None,
+                        help = 'Comma separated entropy components to be used during the oligotyping process.')
+    parser.add_argument('-s', '--min-number-of-datasets', type=int, required=True,
+                        help = 'Minimum number of datasets oligotype expected to appear. The deafult is "5", which\
+                                is another completely arbitrary value. This parameter should be defined based\
+                                on the number of datasets included in the analysis. If there are 10 datasets,\
+                                3 might be a good choice, if there are 5 datasets, 1 would be a better one\
+                                depending on the study.')
+    parser.add_argument('-a', '--min-percent-abundance', type=float, default=0.0,
+                        help = 'Minimum percent abundance of an oligotype in at least one dataset. The default\
+                                is "0.0". Just like --min-number-of-datasets parameter, this parameter too is\
+                                to eliminate oligotypes that are formed by sequencing errors occured at the\
+                                component of interest. The value should be decided based on the average number\
+                                of sequences every dataset has.')
+    parser.add_argument('-A', '--min-actual-abundance', type=int, default=0,
+                        help = 'Minimum total abundance of an oligotype in all datastes. The default\
+                                is "0". If the total abundance of an oligotype is smaller than the number given\
+                                with this parameter, oligotype would be eliminated and not included in downstream\
+                                analyses.')
+    parser.add_argument('-t', '--dataset-name-separator', type=str, default='_',
+                        help = 'Character that separates dataset name from unique info in the defline. For insatnce\
+                                if the defline says >dataset-1_GD7BRW402IVMZE, the separator should be set to "_"\
+                                (which is the default character).')
+    parser.add_argument('-l', '--limit-representative-sequences', type=int, default=None,
+                        help = 'At the end of the oligotyping sequences that are being represented by the same\
+                                oligotype are being uniqued and stored in separate files. The number of sequences\
+                                to keep from the frequency ordered list can be defined with this parameter (e.g.\
+                                -l 10 would make it possible that only first 10 sequence would be stored). Default\
+                                is 0, which stores everything, but when the dataset size is too big, this could\
+                                take up disk space.')
+    parser.add_argument('--limit-oligotypes-to', type = str, default = None,
+                        help = 'Comma separated list of oligotypes to be taken into account during the analysis.\
+                                All other oligotypes will be discarded if a list of oligotypes is being speficied\
+                                with this parameter.')
+    parser.add_argument('--quick', action = 'store_true', default = False,
+                        help = 'Some relatively insignificant parts of the analysis may take a lot of time, such as\
+                                generating figures for representative sequences. When this parameter is set, all\
+                                trivial steps would be skipped to give results as soon as possible.')
+    parser.add_argument('--no-figures', action = 'store_true', default = False,
+                        help = 'When set, no figures will be generated or displayed.')
+    parser.add_argument('--blast-ref-db', default = None, type=str,
+                        help = 'When set, BLAST search will be done locally against the ref db (local BLAST search\
+                                requires USEARCH)')
+    parser.add_argument('--skip-blast-search', action = 'store_true', default = False,
+                        help = 'When set, BLAST search step will not be performed.')
+    parser.add_argument('--no-display', action = 'store_true', default = False,
+                        help = 'When set, no figures will be shown.')
+    parser.add_argument('--gen-html', action = 'store_true', default = False,
+                        help = 'Generate static HTML output to browse analysis results.')
+    parser.add_argument('--gen-dataset-oligo-networks', action = 'store_true', default = False,
+                        help = 'Generate oligotype network structure figures for each dataset.')
+    parser.add_argument('--project', default = None, type=str,
+                        help = 'When a project name is set, given name will be used in figures whenever possible.')
+
+    return parser
+
+
+def entropy():
+    parser = argparse.ArgumentParser(description='Entropy Analysis')
+    parser.add_argument('alignment', metavar = 'ALIGNMENT', help = 'Alignment file\
+                         that contains all datasets and sequences in FASTA format')
+    parser.add_argument('--qual-scores-file', metavar = 'QUAL SCORES FILE',
+                        help = 'FASTA formatted file that contains PHRED base call values\
+                         for each read in the alignment file')
+    parser.add_argument('--qual-scores-dict', metavar = 'QUAL SCORES DICT',
+                        help = 'Previously computed and serialized dictionary that contains\
+                        PHRED base call values for each read in the alignment file. If you\
+                        provide --qual-scores-file, that file will be used to recompute this\
+                        dictionary and the file you refer with this parameter will\
+                        not be ignored')
+    parser.add_argument('--qual-stats-dict', metavar = 'QUAL STATS DICT',
+                        help = 'Previously computed and serialized dictionary that contains\
+                        PHRED base call quality score statistics for the alignment file. If\
+                        you provide --qual-scores-dict, it will be used to recompute this\
+                        dictionary and the file you refer to with this parameter will\
+                        actually not be used')
+    parser.add_argument('--weighted', action = 'store_true', default = False,
+                        help = 'When set, entropy computation per column will use\
+                        mean quality score for each column.')
+    parser.add_argument('--quick', action = 'store_true', default = False,
+                        help = 'When set, entropy values will be shown as fast as\
+                                possible (some visualization steps will be skipped).')
+    parser.add_argument('--no-display', action = 'store_true', default = False,
+                                help = 'When set, no figures will be shown.')
+
+    return parser
