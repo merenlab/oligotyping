9f182c531512e2f0dde78d7afee62e8f9d8ddd35
A. Murat Eren <a.murat.eren@gmail.com>
2012-07-12 21:27:27 -0400

    a little more user-friendly

--- scripts/create_GG_alignment_template_from_taxon.py
+++ scripts/create_GG_alignment_template_from_taxon.py
@@ -1,29 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2010 - 2012, A. Murat Eren
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+
 import os
 import sys
+import argparse
 
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../lib'))
-import fastalib as u
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+import lib.fastalib as u
 
-taxon = sys.argv[1]
+def gen_tmpl(taxon, otu_id_to_greengenes, greengenes_alignment, output_file_path = None):
+    ids = []
+    
+    for id, tax in [line.strip().split('\t') for line in open(otu_id_to_greengenes).readlines()]:
+        if tax.find(taxon) > 0:
+            ids.append(id)
+    
+    ids = list(set(ids))
+    print '%d ids found for %s.' % (len(ids), taxon)
+    
+    template = u.FastaOutput('%s.tmpl' % taxon)
+    fasta = u.SequenceSource(greengenes_alignment)
+    while fasta.next():
+        if fasta.id in ids:
+            template.store(fasta, split = False)
+            ids.remove(fasta.id)
+    
+    fasta.close()
+    template.close()
 
-PATH_TO_OTU_ID_TO_GREENGENES_FILE = '/Users/meren/Desktop/MBL/Oligotyping/GreenGenes/otu_id_to_greengenes.txt'
-PATH_TO_GREENGENES_ALIGNMENT_FILE = '/Users/meren/Desktop/MBL/Oligotyping/GreenGenes/gg_97_otus_6oct2010_aligned.fasta'
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Create GreenGenes Alignment Template')
+    parser.add_argument('taxon', metavar = 'TAXON',
+                        help = '"Taxon" name to be searched in GreenGenes')
+    parser.add_argument('otu_id_to_greengenes', metavar = 'OTU_ID_TO_GREENGENES',
+                        help = 'Path to the "otu_id_to_greengenes" file. You can download it from \
+                               "http://greengenes.lbl.gov/Download/OTUs/gg_otus_6oct2010/taxonomies/otu_id_to_greengenes.txt"')
+    parser.add_argument('greengenes_alignment', metavar = 'GREENGENES_ALIGNMENT',
+                        help = 'Path to the GreenGenes alignment file. You can download it from \
+                               "http://greengenes.lbl.gov/Download/OTUs/gg_otus_6oct2010/rep_set/gg_97_otus_6oct2010_aligned.fasta"')
+    parser.add_argument('-o', '--output', help = 'Output file name', default = None)
 
-ids = []
 
-for id, tax in [line.strip().split('\t') for line in open(PATH_TO_OTU_ID_TO_GREENGENES_FILE).readlines()]:
-    if tax.find(taxon) > 0:
-        ids.append(id)
-
-ids = list(set(ids))
-print '%d ids found for %s.' % (len(ids), taxon)
-
-template = u.FastaOutput('%s.tmpl' % taxon)
-fasta = u.SequenceSource(PATH_TO_GREENGENES_ALIGNMENT_FILE)
-while fasta.next():
-    if fasta.id in ids:
-        template.store(fasta, split = False)
-        ids.remove(fasta.id)
-
-fasta.close()
-template.close()
+    args = parser.parse_args()
+    gen_tmpl(args.taxon, args.otu_id_to_greengenes, args.greengenes_alignment, args.output)
