merenlab/oligotyping

commit 6dd6aae158dd85bcbcd0d5f4bca713291b6635a5

division by 0 error fixed

lib/oligotyping.py
@@ -439,10 +439,10 @@ class Oligotyping:
         self.progress.append('done')
 
         datasets_to_remove = []
-        for i in range(0, len(self.datasets)):
+        for i in range(1, len(self.datasets)):
             dataset = self.datasets[i]
 
-            self.progress.update('Analyzing datasets: ' + P(i, len(self.datasets) - 1))
+            self.progress.update('Analyzing datasets: ' + P(i, len(self.datasets)))
             
             for oligo in datasets_dict_copy[dataset]:
                 if oligo not in self.abundant_oligos:

commit a9877604d295bf8cd7a3351f48719e75a006d223

I think I deserve a medal for 6dd6aae158dd85bcbcd0d5f4bca713291b6635a5. thanks to my poor unittests, I fixed a critical bug I introduced with commit 6dd6aae158dd85bcbcd0d5f4bca713291b6635a5. thank you unittests!

lib/oligotyping.py
@@ -443,10 +443,10 @@ class Oligotyping:
         self.progress.append('done')
 
         datasets_to_remove = []
-        for i in range(1, len(self.datasets)):
+        for i in range(0, len(self.datasets)):
             dataset = self.datasets[i]
 
-            self.progress.update('Analyzing datasets: ' + P(i, len(self.datasets)))
+            self.progress.update('Analyzing datasets: ' + P(i + 1, len(self.datasets)))
             
             for oligo in datasets_dict_copy[dataset]:
                 if oligo not in self.abundant_oligos: