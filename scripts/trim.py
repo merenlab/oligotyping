ae9af7e843d13588259df47000f20947b1ef37bf
A. Murat Eren <a.murat.eren@gmail.com>
2012-08-10 11:16:00 -0400

    an embarrassingly useless trimming script .. I promise I will offer a better one when I have time

---
+++ scripts/trim.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../lib'))
+import fastalib as u
+
+fasta = u.SequenceSource(sys.argv[1], lazy_init = False)
+output = u.FastaOutput(sys.argv[1] + '-TRIMMED')
+
+trim_from = int(sys.argv[2])
+trim_to = int(sys.argv[3]) if len(sys.argv) == 4 else None
+
+while fasta.next():
+    output.write_id(fasta.id)
+    if trim_to:
+        output.write_seq(fasta.seq[trim_from:trim_to], split = False)
+    else:
+        output.write_seq(fasta.seq[trim_from:], split = False)
+
+fasta.close()
+output.close()
