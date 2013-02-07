9b2d26eb79e707be4951a095a6b2b29bdee58d58
A. Murat Eren <a.murat.eren@gmail.com>
2013-02-07 10:40:05 -0500

    this probably is slightly smarter than the other

--- Scripts/R/cluster-analysis.R
+++ Scripts/R/cluster-analysis.R
@@ -35,6 +35,9 @@ rownames(csv) <- csv[,1]
 d <- vegdist(csv[,-1], method=distance) #"manhattan", "euclidean", "canberra", "bray", "kulczynski", "jaccard", "gower", "morisita", "horn", "mountford", "raup" , "binomial" or "chao"
 fit <- hclust(d, method="ward") # "ward", "single", "complete", "average", "mcquitty", "median" or "centroid"
 
+pdf_w = 5 + (length(rownames(csv)) / 10)
+png_w = pdf_w * 100
+
 P <- function(){
     plot(fit, labels=rownames(csv), cex = 0.7, main = title_text, sub = paste("Distance metric: ",distance,sep=""), xlab = '') # display dendogram
 }
@@ -46,14 +49,14 @@ if(display == TRUE){
 
 # PDF
 pdf_output <- paste(output_file_prefix,".pdf",sep="")
-pdf(pdf_output)
+pdf(pdf_output, width = pdf_w, pointsize = 8, family = 'Helvetica')
 P()
 sprintf("Clustering result PDF: '%s'", pdf_output)
 dev.off()
 
 # PNG
 png_output <- paste(output_file_prefix,".png",sep="")
-png(png_output, width = 1200, height = 1000, units = "px", pointsize = 12, bg = "transparent", type = c("cairo", "cairo-png", "Xlib", "quartz"))
+png(png_output, width = png_w, height = 1000, units = "px", pointsize = 12, bg = "transparent", type = c("cairo", "cairo-png", "Xlib", "quartz"))
 P()
 sprintf("Clustering result PNG: '%s'", png_output)
 dev.off()
