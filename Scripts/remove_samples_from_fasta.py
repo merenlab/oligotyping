1ebe04db57eb6387f8227706cdc5182dbc1f27b6
A. Murat Eren <a.murat.eren@gmail.com>
2013-05-15 11:17:31 -0400

    fixy.

--- Scripts/remove_samples_from_fasta.py
+++ Scripts/remove_samples_from_fasta.py
@@ -4,7 +4,6 @@
 #
 
 import sys
-import operator
 
 import Oligotyping.lib.fastalib as u
 from Oligotyping.utils.utils import pretty_print as pp
@@ -15,7 +14,7 @@ samples_to_be_removed = sys.argv[2].split(',')
 
 while fasta.next():
     if fasta.pos % 1000 == 0:
-        sys.stderr.write('\rreads processed so far: %d' % (fasta.pos))
+        sys.stderr.write('\rreads processed so far: %s' % (pp(fasta.pos)))
         sys.stderr.flush()
     sample_name = '_'.join(fasta.id.split('_')[:-1])
 
