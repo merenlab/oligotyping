3d208fde3208f11d4e43cbe5647eabb09ab86a2d
A. Murat Eren <a.murat.eren@gmail.com>
2014-08-15 19:18:43 -0400

    forgotten static files are in the distribution now

--- setup.py
+++ setup.py
@@ -9,23 +9,19 @@ if os.environ.get('USER','') == 'vagrant':
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
 install_reqs = parse_requirements('requirements.txt')
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name = "Oligotyping",
+    name = "oligotyping",
     version = "1.0",
-    description = "Oligotyping and minimum entropy decomposition (MED) pipeline for the analysis of marker gene amplicons",
+    description = "The oligotyping and minimum entropy decomposition (MED) pipeline for the analysis of marker gene amplicons",
     author = u"A. Murat Eren",
     author_email = "meren@mbl.edu",
     license = "GPLv3+",
     url = "http://oligotyping.org",
     packages = find_packages(),
 
-    longer_description=README,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -35,12 +31,14 @@ setup(
         'Natural Language :: English',
         'Operating System :: MacOS',
         'Operating System :: POSIX',
-        'Programming Language :: R',
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering',
     ],
 
     scripts = [script for script in glob.glob('bin/*') if not script.endswith('-OBSOLETE')],
+
+    include_package_data = True,
+    package_data={'': ['Oligotyping/utils/html/scripts/*', 'Oligotyping/utils/html/static/*', 'Oligotyping/utils/html/templates/*']},
 
     install_requires=reqs,
 )
