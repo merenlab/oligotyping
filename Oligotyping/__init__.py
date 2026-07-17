b44ae6fed37f12335b09af532f7a957a0d9ceb52
A. Murat Eren (Meren) <a.murat.eren@gmail.com>
2026-07-17 14:46:58 +0200

    update init

--- Oligotyping/__init__.py
+++ Oligotyping/__init__.py
@@ -2,14 +2,13 @@
 
 import os
 import sys
-import pkg_resources
 
 # Make sure the Python environment hasn't changed since the installation (happens more often than you'd think
 # on systems working with multiple Python installations that are managed through modules):
 try:
-    if sys.version_info < (3, 6, 0):
+    if sys.version_info < (3, 7, 0):
         v =  '.'.join([str(x) for x in sys.version_info[0:3]])
-        sys.stderr.write("Your active Python version is '%s'. Anything less than '3.6.0' will not do it for the oligotyping pipeline :/\n" % v)
+        sys.stderr.write("Your active Python version is '%s'. Anything less than '3.7.0' will not do it for the oligotyping pipeline :/\n" % v)
         sys.exit(-1)
 except Exception:
     sys.stderr.write("(oligotyping pipeline failed to learn about your Python version, but it will pretend as if nothing happened)\n\n")
@@ -20,12 +19,16 @@ run = Run()
 
 def set_version():
     try:
-        __version__ = pkg_resources.require("oligotyping")[0].version
-    except:
+        # `importlib.metadata` is a part of the standard library as of Python 3.8, and is the
+        # modern replacement for the deprecated `pkg_resources` (which is no longer installed
+        # by default with recent Python / setuptools versions).
+        from importlib.metadata import version
+        __version__ = version("oligotyping")
+    except Exception:
         # maybe it is not installed but being run from the codebase dir?
         try:
             __version__ = open(os.path.normpath(os.path.dirname(os.path.abspath(__file__))) + '/../VERSION').read().strip()
-        except:
+        except Exception:
             __version__ = 'unknown'
 
     return __version__
